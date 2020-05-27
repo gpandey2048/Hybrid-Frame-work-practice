cc6f885547425cf356f091b94d9d2461c0953d21
gpandey2048 <65774195+gpandey2048@users.noreply.github.com>
2020-05-27 11:24:56 -0400

    data and keyword works

---
+++ HybridFramework/src/test/java/com/newMavenProject/BaseClass/BaseTest.java
@@ -0,0 +1,33 @@
+package com.newMavenProject.BaseClass;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class BaseTest {
+	public static String projectPath=System.getProperty("user.dir");
+	public static FileInputStream fis;
+	public static Properties baseProperties;
+	public static Properties childProperties;
+
+public void init() throws Exception {
+	
+ fis= new FileInputStream(projectPath+"//src//test//resources//environment.properties");
+	Properties baseProperties= new Properties();
+	baseProperties.load(fis);
+	String envType = baseProperties.getProperty("env");
+	System.out.println(envType);
+	
+	fis=new FileInputStream(projectPath+"//src//test//resources//"+envType+".properties");
+	childProperties= new Properties();
+	childProperties.load(fis);
+	
+	String url = childProperties.getProperty("zohourl");
+	System.out.println(url);
+	
+	
+	
+}
+
+
+
+}
---
+++ HybridFramework/src/test/java/com/newMavenProject/SuiteA/TC_001.java
@@ -0,0 +1,34 @@
+package com.newMavenProject.SuiteA;
+
+import org.testng.annotations.Test;
+
+import com.newMavenProject.BaseClass.BaseTest;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class TC_001 extends BaseTest {
+ 
+  @BeforeTest
+  public void startProcess() throws Exception {
+	  init();
+
+  }
+  
+ 
+
+
+@Test
+  public void f() {
+	  
+	  
+	  
+  }
+
+  @AfterTest
+  public void endProcess() {
+	  
+	  
+  }
+
+}
